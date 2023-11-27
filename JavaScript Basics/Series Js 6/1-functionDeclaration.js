2b1120dcc7c10d430d5343fe12fd3a744f52be4b
mkkukul <mkkukul@gmail.com>
2023-11-27 07:30:49 +0300

    27.11.2023

---
+++ JavaScript Basics/Series Js 6/1-functionDeclaration.js
@@ -0,0 +1,55 @@
+// ? ===============================================
+// ?               FONKSİYONLAR
+// ? ===============================================
+
+// !------------------------------------------------
+// ! 1.YONTEM  : FUNCTION DECLARATION
+// !------------------------------------------------
+
+console.log("**** FUNC DECLARATION ******");
+
+//* ORNEK:
+//************************************************/
+yazdir();
+
+//! Fonksiyonun tanimlanmasi (declaration)
+
+
+
+
+
+
+//* ORNEK2:
+//*************************************************/
+
+
+//! Bir parametreyi cagirma sirasinda kullanmaz isek onun
+//! yerine default parametre atayabiliriz. Ornekteki lastName
+//! parametresi icin default değer olarak '' atanmistir.
+
+//* ORNEK3:
+//***************************************************/
+
+
+
+
+
+
+
+
+
+
+
+//* ORNEK4:
+//*****************************************************/
+
+
+
+
+
+
+
+//* ORNEK4:
+//*****************************************************/
+
+
---
+++ JavaScript Basics/Series Js 6/2-functionExpression.js
@@ -0,0 +1,42 @@
+// ? ================================================
+// ?                FONKSİYONLAR
+// ?=================================================
+
+//!--------------------------------------------------
+//! 2.YONTEM  : FUNCTION EXPRESSION
+//!--------------------------------------------------
+
+console.log("******** 2- EXPRESSION*******");
+
+//* ORNEK:
+//***************************************************/
+// function tekCift(sayi) {
+//   return sayi % 2 ? `${sayi} TEKTIR` : `${sayi} CIFTTIR`;
+// }
+
+//! Cannot access 'tekCift1' before initialization  at
+// console.log(tekCift1(9));
+
+//! Funct Expression yontemi ile tanimlama
+
+
+//* ORNEK: 3 sayinin en büyüğünü bulan fonks.
+//******************************************************/
+
+
+
+
+
+
+
+// const n1 = +prompt("Sayi1:");
+// const n2 = +prompt("Sayi2:");
+// const n3 = +prompt("Sayi3:");
+
+// console.log(Number.MIN_VALUE);
+// console.log("Girilen sayilarin en buyugu:", buyukBul(n1, n2, n3));
+
+
+
+//* ORNEK: Bir fonksiyon içerisinde başka fonksiyonlar çağrılabilir
+//**************************************************/
