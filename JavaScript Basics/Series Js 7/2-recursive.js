24d8c5aa287d16dc556ceefae4e873e617bdd763
mkkukul <mkkukul@gmail.com>
2023-11-30 06:56:27 +0300

    x

--- JavaScript Basics/Series Js 7/2-recursive.js
+++ JavaScript Basics/Series Js 7/2-recursive.js
@@ -16,10 +16,22 @@ console.log("****** 2 - RECURSION ********");
 //! f(n) = f(n-1) + f(n-2)
 
 const fibo = (n) =>{
-  fibo(n-1)
+  if ( n<= 0) {
+    return 0;
+    } else if ( n === 1  || n===2 ) {
+      return 1;
+  } else {
+    return fibo (n-1)+ fibo(n-2)
+  }
 
+};
+
+const n = +prompt("n terimini giriniz");
+if (n === 1|| n === 2 ) {
+  console.log("lutfen 0 dan buyuk bir sayi giriniz")
+} else {
+  console.log(`FIBO(${n})= ${fibo(n)}`);
 }
-const n = 
 
 
 
