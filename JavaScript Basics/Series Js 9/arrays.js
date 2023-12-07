719d3e675e1764aa9645d1ba8d0e9fad6b9bd978
mkkukul <mkkukul@gmail.com>
2023-12-07 11:16:24 +0300

    son ders X

--- JavaScript Basics/Series Js 9/arrays.js
+++ JavaScript Basics/Series Js 9/arrays.js
@@ -60,13 +60,24 @@ console.log(isimler);
 // isimler = ["Ali", "Veli"];
 
 //? numbers dizisinin son elementinin degerini bir arttirin
+console.log(numbers);
+numbers[numbers.length -1]++;
+console.log(numbers);
 
 //? kisiler icerisindeki yaslar dizisinde bulunan 55 yasini konsola yazdiriniz.
-
+const yaslar = [22, 44, 55];
+const kisiler = ["Ahmet", "Can", 2022, 2022 - 1990, true, yaslar];
+console.log(kisiler);
+const yas55 = kisiler[5][2];
+// const yas55 = kisiler[5][2];
+console.log(yas55);
 
 //? kisiler icerisindeki yaslar dizisinde bulunan 44 yasini bir eksiltiniz.
 
-
+kisiler[5][1]--;
+console.log(kisiler);
+// !
+console.log(yaslar);
 
 //? ===========================================================
 //?  DIZIYI DEGISTIREN (MUTATOR) METOTLAR
@@ -74,37 +85,55 @@ console.log(isimler);
 
 
 //** pop() son elemanı siler ve sildigi elemani dondurur.
-
+const arabalar = ["BMW", "MERCEDES", "Fiat", "Anadol"];
+// arabalar.pop();
+const silinen =arabalar.pop();
+console.log(arabalar,silinen);
 
 
 //* push() dizinin sonuna eleman ekler ve dizinin son eleman sayisini dondurur.
 
+const n = arabalar.push("Citroen");
+console.log(arabalar,n);
 
 
 
 //* unshift dizinin 0. indeksine eleman ekler ve dizinin son eleman sayisini dondurur.
-
+const  n1 = arabalar.unshift("Audi");
+console.log(arabalar,n1);
 
 
 //* dizinin 0. indeks elemanini siler ve silenen elemani dondurur
-
+const Audi = arabalar.shift();
+console.log(arabalar,Audi);
 
 
 //* splice()
+arabalar.splice(1,0,"Passat");
+console.log(arabalar);
 //? 1.parametre: eklenecek indis numarasi
 //? 2.parametre: 0 ise araya ekleme, 1 ise uzerine yazma
 //? 3.parametre: yeni eklenecek veri
-
+// arabalar.splice(3,1,"Honda","Toyota"); birden fala eklenebilir
+arabalar.splice(3,1,"Honda");
+console.log(arabalar);
 
 
 //* Dizinin tamamini ters siraya cevirir.
+arabalar.reverse();
+console.log(arabalar);
 
 
-//* sort() Diziyi alfabetik olarak siralar.
+//* sort() Diziyi alfabetik olarak siralar. hizlama sayiları sıralamak için kullanılmaz
+isimler.sort()
+console.log(isimler)
+const sayilar = [2, 3, 4, 11, 22, 5, 6, 7]
+sayilar.sort();
+console.log(sayilar);
+ sayilar.sort((a,b) => a-b);
+ console.log(sayilar);
 
-
-
-//! Alfabetik siraladigi icin dogru sonuc cikmayabilir.
+//! Alfabetik siraladigi icin dogru sonuc cikmayabilir. başka fonsiyonmantıgı oluşturulabilir
 
 
 
@@ -115,7 +144,14 @@ console.log(isimler);
 
 
 //* fill()
+const array1 = [1, 2, 3, 4];
+array1.fill(0);
+console.log(array1);
 
+array1.fill(1, 2, 4);
+console.log(array1);
+array1.fill(-1, 1); //* 1. eleman ve sonrasini -1 yap
+console.log(array1);
 //? ===========================================================
 //?  DIZI ERISIM METOTLARI
 //? ===========================================================
