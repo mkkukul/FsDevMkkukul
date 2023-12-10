8a72f9fa61b2197a1dd89183128bc89feb3fd4d0
mkkukul <mkkukul@gmail.com>
2023-12-10 19:37:10 +0300

    first night

--- JavaScript Basics/Series Js 10/2-iteration.js
+++ JavaScript Basics/Series Js 10/2-iteration.js
@@ -7,10 +7,32 @@ console.log("****** ITERATION **********");
 //
 //?-------------- ÖRNEK -------------------
 //?  Dizideki herbir fiyati konsola bastiriniz.
+
+const prices = [100, 250, 50, 89];
+prices.forEach((p) => console.log(p));
+
+//? ORNEK: prices dizisindekilerin toplamini konsola yazdiriniz */
+let sum = 0;
+prices.forEach((price) => (sum += price));
+console.log("SUM:", sum);
+
+//! NOT: forEach metodu void metottur.(Herhangi bir deger dondurmez)
+console.log(prices.forEach((price) => (sum += price))); //! undefined
+
 //?-------------- ÖRNEK -------------------
 //? prices dizisindeki her bir ara toplam degerini
 //? konsola bastiriniz. Ayrica her bir fiyata %10 zam yapiniz.
-/* ======================================================
+
+let total = 0;
+prices.forEach((price, index, arr) => {
+  total += price;
+  console.log(`${index + 1}.iteration: ${total}`);
+  arr[index] = Math.trunc(price * 1.1);
+});
+
+console.log(prices);
+
+//* ======================================================
 //*                       MAP METHOD
 //* ======================================================
 
