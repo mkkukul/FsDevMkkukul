d001ef7f029053b437d3ea920d15b9c30c403328
mkkukul <mkkukul@gmail.com>
2023-12-10 19:34:20 +0300

    First Dance

---
+++ JavaScript Basics/Series Js 10/1-loops.js
@@ -0,0 +1,31 @@
+//* =====================================================
+//*                     FOR LOOP
+//* ======================================================
+
+console.log("******* LOOPS IN ARRAYS ******");
+
+//?-------------- ÖRNEK -------------------
+
+//?-------------- ÖRNEK -------------------
+//? Dizideki notlarin ortalamasini hesaplayiniz.
+
+//* ======================================================
+//*                   FOR-IN LOOP
+//* ======================================================
+
+//*-------------------------------------------------------
+//* SORU: Ogrenciler dizisinde ogrenci isimleri saklanmaktadir.
+//* ogrencileri aramamizi saglayacak ve aranilan ogrenciden
+//* ne kadar sayida bulunuldugunu  ana programa döndürecek bir
+//* fonksiyonu yaziniz. Eger aranilan isimde bir ogrenci yok ise
+//* fonksiyon "ogrenci bulunamadi" dondurulmelidir.
+//*--------------------------------------------------------
+
+
+//* ======================================================
+//*                   FOR-OF LOOP
+//* ======================================================
+
+//* for of dongusu, for in dongusunun bir cok veri yapisini
+//* kapsayacak sekilde guncellenmis halidir. Bu dongude dizi
+//* icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
