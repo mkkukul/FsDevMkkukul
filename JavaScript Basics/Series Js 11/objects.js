18e7b55413bc021bb8cf5b9005be24680ae31043
mkkukul <mkkukul@gmail.com>
2023-12-29 22:47:11 +0300

    78879

--- JavaScript Basics/Series Js 11/objects.js
+++ JavaScript Basics/Series Js 11/objects.js
@@ -18,7 +18,7 @@ console.log("******* Objects *********");
 const arabalar = new Object();
 arabalar.marka = "BMW",
 arabalar.motor = "1.3";
-arabalar
+arabalar.model 
 // const arabalar = new Object();
 // arabalar.marka = "BMW";
 // arabalar.motor = "1.3";
