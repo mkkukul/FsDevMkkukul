6bf772b6553d06eb3fed0278d971ecb1cf4e9b0f
mkkukul <mkkukul@gmail.com>
2024-01-16 10:49:56 +0300

    5

--- JavaScript Basics/Series Js 11/tR.js
+++ JavaScript Basics/Series Js 11/tR.js
@@ -4,3 +4,7 @@ arabalar.motor = "1.3";
 arabalar.model = 2023;
 arabalar.lpg = true;
 console.log(arabalar); // { marka: 'BMW', motor: '
+
+
+console.log(arabalar.lpg)
+console.log("MODEL",arabalar["model"])
