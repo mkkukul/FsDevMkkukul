f5171be5f7862ee1919d04cdc1365acc60adb7f8
mkkukul <mkkukul@gmail.com>
2024-01-16 11:45:43 +0300

    8

--- JavaScript Basics/Series Js 11/tR.js
+++ JavaScript Basics/Series Js 11/tR.js
@@ -1,25 +1,41 @@
-const arabalar = new Object();
-arabalar.marka = "BMW";
-arabalar.motor = "1.3";
-arabalar.model = 2023;
-arabalar.lpg = true;
-console.log(arabalar); // { marka: 'BMW', motor: '
-
-
-console.log(arabalar.lpg)
-console.log("MODEL",arabalar["model"])
-const key = "motor"
-console.log(arabalar[key])
-arabalar.motor = "1.6"
-console.log(arabalar)
-
-
-function Personel(id,ad,maas){
-    this.perId = id;
-    this.perAd = ad;
-    this.maas = 15000;
-    console.log(this)
-}
-const kisi1 = new Personel("1234567890", "Mustafa", 15000);
-const kisi2 = new Personel("1234567800", "Canan", 12000);
+// const arabalar = new Object();
+// arabalar.marka = "BMW";
+// arabalar.motor = "1.3";
+// arabalar.model = 2023;
+// arabalar.lpg = true;
+// console.log(arabalar); // { marka: 'BMW', motor: '
+// 
+// 
+// console.log(arabalar.lpg)
+// console.log("MODEL",arabalar["model"])
+// const key = "motor"
+// console.log(arabalar[key])
+// arabalar.motor = "1.6"
+// console.log(arabalar)
+// 
+// 
+// function Personel(id,ad,maas){
+    // this.perId = id;
+    // this.perAd = ad;
+    // this.maas = 15000;
+    // console.log(this)
+// }
+// const kisi1 = new Personel("1234567890", "Mustafa", 15000);
+// const kisi2 = new Personel("1234567800", "Canan", 12000);
 // console.log(kisi1)
+
+const worker = {
+    name :"Can",
+    surname :"Can",
+    age :33,
+    job :"developer",
+    languages :["C++", "Java", "JS", "Pyhton", "Go"],
+    salary :140000,
+};
+console.log(worker)
+console.log(worker.languages[0])
+console.log("Lang:", worker["languages"])
+worker["languages"].forEach((l)=> console.log(l))
+worker.dob = "1990";
+worker.email ="can@mail.com"
+console.log(worker)
