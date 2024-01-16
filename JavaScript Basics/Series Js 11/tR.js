c467a7ffc241979bf3d18d4d5451c1fd5cb8d97f
mkkukul <mkkukul@gmail.com>
2024-01-16 12:32:23 +0300

    13

--- JavaScript Basics/Series Js 11/tR.js
+++ JavaScript Basics/Series Js 11/tR.js
@@ -73,3 +73,31 @@ const personel = {
 // new Date().getFullYear()
 console.log("DOB:",personel.calculateAge())
 console.log("SUMMARY:", personel.summary());
+
+const people = {
+    person1: {
+        name: "Can",            
+        surname: "Canan",       
+        dob: "1990",            
+        job: "developer",       
+        salary: "140000",       
+        drivingLicense: true    
+    },
+    person2: {
+        name: "John",           
+        surname: "Sweet",       
+        dob: "1990",            
+        job: "tester",          
+        salary: "110000",       
+        drivingLicense: false   
+    },
+    person3: {
+        name: "Steve",          
+        surname: "Job",         
+        dob: "2000",            
+        job: "developer",       
+        salary: "90000",        
+        drivingLicense: true    
+    },
+};
+console.log(people.person1["name"])
