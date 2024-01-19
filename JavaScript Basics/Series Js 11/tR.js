005515787e220cccd66d60f5c08612f2e6fadc99
mkkukul <mkkukul@gmail.com>
2024-01-19 22:31:08 +0300

    15

--- JavaScript Basics/Series Js 11/tR.js
+++ JavaScript Basics/Series Js 11/tR.js
@@ -1,10 +1,10 @@
-// const arabalar = new Object();
-// arabalar.marka = "BMW";
-// arabalar.motor = "1.3";
-// arabalar.model = 2023;
-// arabalar.lpg = true;
-// console.log(arabalar); // { marka: 'BMW', motor: '
-// 
+const arabalar = new Object();
+arabalar.marka = "BMW";
+arabalar.motor = "1.3";
+arabalar.model = 2023;
+arabalar.lpg = true;
+console.log(arabalar); // { marka: 'BMW', motor: '
+
 // 
 // console.log(arabalar.lpg)
 // console.log("MODEL",arabalar["model"])
