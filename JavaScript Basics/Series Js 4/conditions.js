1f785e668c96c5d28cbffed8e062ce03820e87d6
mkkukul <mkkukul@gmail.com>
2024-01-28 22:05:47 +0300

    3

--- JavaScript Basics/Series Js 4/conditions.js
+++ JavaScript Basics/Series Js 4/conditions.js
@@ -25,39 +25,3 @@ console.log(sayi, typeof sayi);
 
 // ? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu yazdırınız
 
-// const n1 = +prompt ("Sayi1:")
-// const n2 = +prompt ("Sayi2:")
-// const n3 = +prompt ("Sayi3:")
-
-// 1. Yontem
-// if ( n1>= n2 && n1>= n3) {
-    // console.log(`${n1} en buyuk sayidir`);
-// }   else if (n2 >= n1 && n2 >= n3) {
-    // console.log(`${n2} en buyuk sayidir`);
-// }   else if (n3 >=n1 && n3 >= n2 ) {
-    // console.log(`${n2} en buyuk sayidir`);
-// } 
-// 
-
-// 2. Yontem
-// let enBuyuk = n1;
-// if (n2 >= enBuyuk) {
-    // enBuyuk = n2;
-// } 
-// if (n3 >= enBuyuk) {
-    // enBuyuk = n3;   
-// }
-// console.log(`${enBuyuk} en buyuktur`);
-
-// const not = +prompt("Not Giriniz");
-// let sonuc = (not >=50 ? "Geçti" : "Kaldi");
-// console.log(sonuc);
-// console.log(not >= 50 ? "Gecti" : "Kaldi");
-const yas = 17;
-const cinsiyet = "erkek";
-const saglikli = true;
-yas >= 18 && cinsiyet === "erkek" && saglikli  
-?console.log("Askerlik yapmali") 
-:console.log("Askerlik yapmasina gerek yok")
-
- 
