49f5fafd7bc8f88a856ba1c21d3ce28ce796eb8d
mkkukul <mkkukul@gmail.com>
2024-01-28 22:06:18 +0300

    4

--- JavaScript Basics/Series Js 4/conditions.js
+++ JavaScript Basics/Series Js 4/conditions.js
@@ -25,3 +25,43 @@ console.log(sayi, typeof sayi);
 
 // ? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu yazdırınız
 
+// Kullanıcıdan üç sayı al
+const n1 = +prompt("Sayı 1:");
+const n2 = +prompt("Sayı 2:");
+const n3 = +prompt("Sayı 3:");
+
+// 1. Yöntem: Her bir sayıyı kontrol et ve en büyük olanı bul
+if (n1 >= n2 && n1 >= n3) {
+    console.log(`${n1} en büyük sayıdır`);
+} else if (n2 >= n1 && n2 >= n3) {
+    console.log(`${n2} en büyük sayıdır`);
+} else if (n3 >= n1 && n3 >= n2) {
+    console.log(`${n3} en büyük sayıdır`);
+}
+
+// 2. Yöntem: En büyük sayıyı bir değişkende tut
+let enBuyuk = n1;
+if (n2 >= enBuyuk) {
+    enBuyuk = n2;
+}
+if (n3 >= enBuyuk) {
+    enBuyuk = n3;
+}
+console.log(`${enBuyuk} en büyüktür`);
+
+// Kullanıcıdan bir not al
+const not = +prompt("Not Giriniz");
+
+// Ternary operatörü kullanarak geçme veya kalma durumunu kontrol et
+let sonuc = not >= 50 ? "Geçti" : "Kaldı";
+console.log(sonuc);
+console.log(not >= 50 ? "Geçti" : "Kaldı");
+
+// Ternary operatörü kullanarak askerlik durumunu kontrol et
+const yas = 17;
+const cinsiyet = "erkek";
+const saglikli = true;
+
+yas >= 18 && cinsiyet === "erkek" && saglikli
+  ? console.log("Askerlik yapmalı")
+  : console.log("Askerlik yapmasına gerek yok");
