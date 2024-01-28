c933fe2aeeac265674f7fd161148fee7f5b48e2d
mkkukul <mkkukul@gmail.com>
2024-01-28 22:10:24 +0300

    10

--- JavaScript Basics/Series Js 5/2-while-dowhile.js
+++ JavaScript Basics/Series Js 5/2-while-dowhile.js
@@ -35,3 +35,18 @@ console.log("****** DO-WHILE *******");
 //? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
 //? not girmeye zorlayan kodu do-while dongusu ile yaziniz.
 
+// Kullanıcıdan 0-100 arasında bir not al
+let not1;
+
+// Do-while döngüsü kullanarak, geçerli bir not girilene kadar devam et
+do {
+    // Kullanıcıdan notu al (prompt fonksiyonuyla alınan değer string olduğu için "+" işlemiyle sayıya çevriliyor)
+    not1 = +prompt("Lütfen 0-100 arasında bir not giriniz.");
+
+    // Eğer not 0-100 arasında değilse, uyarı ver
+    if (not1 < 0 || not1 > 100)
+        console.log("Not 0-100 arasında olmalıdır");
+} while (not1 < 0 || not1 > 100);
+
+// Geçerli bir not girildiğinde notu konsola yazdır
+console.log("Girdiğiniz Not:", not1);
