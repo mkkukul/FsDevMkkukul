76d18cd803e407587fff358df3b08bbabe0bd8b4
mkkukul <mkkukul@gmail.com>
2024-01-28 22:05:35 +0300

    2

--- JavaScript Basics/Series Js 4/conditions.js
+++ JavaScript Basics/Series Js 4/conditions.js
@@ -8,15 +8,20 @@ console.log("****** CONDITIONS *******");
 //? ORNEK1: Console'dan girilen bir sayisinin pozitif,negatif
 //? yada 0 oldugunu tespit ederek yazdiriniz.
 
-// const sayi = prompt ("Bir sayi giriniz:")
-// if (sayi > 0) {
-    // console.log(`${sayi} pozitiftir.`)
-// }else if(sayi ==0){
-    // console.log (`${sayi} 0'a esittir.`)
-// }else {
-    // console.log(`${sayi} negatiftir.`)
-// } 
-// console.log(sayi, typeof sayi);
+// Kullanıcıdan bir sayı al
+const sayi = prompt("Bir sayı giriniz:");
+
+// Girilen sayının pozitif, negatif veya sıfır olduğunu kontrol et
+if (sayi > 0) {
+    console.log(`${sayi} pozitiftir.`);
+} else if (sayi == 0) {
+    console.log(`${sayi} 0'a eşittir.`);
+} else {
+    console.log(`${sayi} negatiftir.`);
+}
+
+// Girilen sayının değerini ve türünü konsola yazdır
+console.log(sayi, typeof sayi);
 
 // ? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu yazdırınız
 
