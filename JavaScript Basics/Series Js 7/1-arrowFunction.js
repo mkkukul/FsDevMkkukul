462ee26c231868e592fff583e31786c1a9f48698
mkkukul <mkkukul@gmail.com>
2024-01-29 20:04:45 +0300

    8

--- JavaScript Basics/Series Js 7/1-arrowFunction.js
+++ JavaScript Basics/Series Js 7/1-arrowFunction.js
@@ -123,29 +123,6 @@ console.log("YASINIZ (Declaration): " + yasHesapla3(tarih));
 //? FIBONACCI terimleri:  1, 1, 2, 3, 5, 8, 13, 21, ...
 
 
-let stringResult ="1,1";
-const fibo = (n) => {
-  let fib1 = 1;
-  let fib2 = 1;
-  let toplam = 0;
-
-  for(let i = 2 ; i < n; i++) {
-    toplam = fib1 + fib2;
-    fib1 = fib2;
-    fib2 = toplam;
-    stringResult += ","+ fib2;
-     
-  }
-  return fib2;
-
-}; 
-const n = +prompt("n terimini giriniz:")
-if (n<=0) {
-  console.log("Lutfen 0 dana buyuk bir sayi giriniz");
-} else{
-  console.log(`FIBO(${n})= ${fibo(n)}`);
-  console.log(stringResult);
-}
 
   
 
