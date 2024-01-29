41f890bfc310b12ca0214e18b80a1704c2484910
mkkukul <mkkukul@gmail.com>
2024-01-29 19:39:30 +0300

    2

--- JavaScript Basics/Series Js 7/1-arrowFunction.js
+++ JavaScript Basics/Series Js 7/1-arrowFunction.js
@@ -10,6 +10,20 @@
 // 
 //* ORNEK: Silindir Hacmi
 //************************************************/
+// 
+// const r =Number(prompt("yaricap giriniz"))
+// const h =Number(prompt("yukseklik giriniz"))
+// const hacimHesapla = (r,h) => Math.PI*r*h;
+// console.log(`${r},${h}=> Hacmi: ${hacimHesapla(r,h).toExponential}  `);
+// // console.log(`${r} , ${h} => Hacmi: ${hacimHesapla (r,h).toFixed(2)}`);
+
+// const r = Number(prompt("yaricap giriniz:"));
+// const h = Number(prompt("yukseklik giriniz:"));
+
+// const hacimHesapla = (r, h) => Math.PI * r * r * h;
+
+// // console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toExponential()}`);
+// console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toFixed(2)}`);
 
 
 // //* ORNEK: Yas hesapla
