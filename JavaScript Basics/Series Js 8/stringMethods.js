f027e5d0ec0ac0858237273fe8652c2677beec01
mkkukul <mkkukul@gmail.com>
2024-01-29 20:13:31 +0300

    16

--- JavaScript Basics/Series Js 8/stringMethods.js
+++ JavaScript Basics/Series Js 8/stringMethods.js
@@ -5,21 +5,18 @@
 
 console.log("*** STRING METHODS ***");
 
-//!1- Klasik yöntem (String Literals) ile string tanimlanirsa bu string
-//! bir primitive degiskendir.
-let str1 = "Clarusway";
-//Char => C, l, a,....
-const str2 = " Hello";
-const str3 = " Full Stack";
 
-console.log(str1, typeof str1);
 
-//!2 - Non-primitive String tanımlama
-const str4 = new String("Non-Primitive");
-console.log(typeof str4);
 
-//?----------------------------------------
-console.log(str1.toLowerCase());
+
+
+
+
+
+
+
+
+
 
 //* Normalde primitive veri tiplerinin property veya metodu olmaz.
 //* Ancak,Javascript otomatik olarak primitive String'ler için object wrapper oluşturur.
