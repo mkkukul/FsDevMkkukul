b8883c15ccce0b85ec38cadd1ad37fb2f6e5d18b
mkkukul <mkkukul@gmail.com>
2024-02-04 20:29:44 +0300

    15

--- JavaScript Basics/Series Js 10/2-iteration.js
+++ JavaScript Basics/Series Js 10/2-iteration.js
@@ -9,6 +9,8 @@ console.log("****** ITERATION **********");
 //?-------------- ÖRNEK -------------------
 //?  Dizideki herbir fiyati konsola bastiriniz.
 
+
+
 // const prices = [100, 250, 50, 89];
 // prices.forEach((p) => console.log(p));
 
