1d241fd2afd485700b465b59cb48f2e9afc91452
mkkukul <mkkukul@gmail.com>
2024-02-04 20:32:39 +0300

    20

--- JavaScript Basics/Series Js 10/2-iteration.js
+++ JavaScript Basics/Series Js 10/2-iteration.js
@@ -48,6 +48,8 @@ console.log(prices.forEach((price) => (sum += price))); // undefined
 //?-------------- ÖRNEK -------------------
 //? prices dizisindeki her bir ara toplam degerini
 //? konsola bastiriniz. Ayrica her bir fiyata %10 zam yapiniz.
+
+
 // let total = 0;
 // prices.forEach((price, index, arr) => {
   // total += price;
@@ -56,14 +58,14 @@ console.log(prices.forEach((price) => (sum += price))); // undefined
 // });
 // console.log(prices);
 
-let total = 0;
-prices.forEach((price, index, arr) => {
-  total += price;
-  console.log(`${index + 1}.iteration: ${total}`);
-  arr[index] = Math.trunc(price * 1.1);
-});
-
-console.log(prices);
+// let total = 0;
+// prices.forEach((price, index, arr) => {
+  // total += price;
+  // console.log(`${index + 1}.iteration: ${total}`);
+  // arr[index] = Math.trunc(price * 1.1);
+// });
+// 
+// console.log(prices);
 // ! 1 ile 1.1  arasında % 10 Fark var
 
 //* ======================================================
