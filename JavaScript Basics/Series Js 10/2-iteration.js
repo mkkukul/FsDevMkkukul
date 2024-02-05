6d698e4d55f5517054e8d4125573a234370fdcc0
mkkukul <mkkukul@gmail.com>
2024-02-05 19:46:18 +0300

    22

--- JavaScript Basics/Series Js 10/2-iteration.js
+++ JavaScript Basics/Series Js 10/2-iteration.js
@@ -246,6 +246,15 @@ const salaries = [5500, 8000, 6500, 9000, 10000, 15000, 25000];
 //?-------------- ÖRNEK -------------------
 //? Maasi 10000'den buyuk olanlari ayri bir diziye saklayalim
 
+// Maaş dizisinden 10000'den büyük olanları filtreleme
+const bigThan = salaries.filter((s) => s > 10000);
+console.log("10000'den büyük maaşlar:", bigThan, "Orjinal maaşlar:", salaries);
+
+// Maaş dizisinden 6000 ile 10000 arasındakileri filtreleme
+const range = salaries.filter((s) => s >= 6000 && s <= 10000);
+console.log("6000 ile 10000 arasındaki maaşlar:", range);
+
+
 // const bigThan = salaries.filter((s) => s > 10000);
 // console.log(bigThan, salaries);
 // 
