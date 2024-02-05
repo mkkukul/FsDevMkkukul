70e9e8db1719ba6fd0f7a98010bda7783fe2b866
mkkukul <mkkukul@gmail.com>
2024-02-05 19:40:35 +0300

    13

--- JavaScript Basics/Series Js 10/2-iteration.js
+++ JavaScript Basics/Series Js 10/2-iteration.js
@@ -133,9 +133,9 @@ console.log(names.map((name) => name.toUpperCase()));
 
 
 
-const euro = 18.23;
-const dolar = 18.19;
-const tlPrices = [100, 150, 100, 50, 80];
+// const euro = 18.23;
+// const dolar = 18.19;
+// const tlPrices = [100, 150, 100, 50, 80];
 
 // ! toPrecision sayı string yapar burada son 3 kısmı gösterir
 
