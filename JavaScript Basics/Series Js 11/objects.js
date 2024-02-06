308c8281c099bb4cbc994c99050f5aa753956bda
mkkukul <mkkukul@gmail.com>
2024-02-06 16:45:55 +0300

    10

--- JavaScript Basics/Series Js 11/objects.js
+++ JavaScript Basics/Series Js 11/objects.js
@@ -1,34 +1,35 @@
 
-//* ---------------------------------------------------------
-//* 3- Object literal (En cok tercih edilen yontem)
-//* ---------------------------------------------------------
+// //* ---------------------------------------------------------
+// //* 3- Object literal (En cok tercih edilen yontem)
+// //* ---------------------------------------------------------
 
-const worker = {
-  name: "Can",
-  surname: "Canan",
-  age: 33,
-  job: "developer",
-  languages: ["C++", "Java", "Javacript", "Pyhton", "Go"],
-  salary: "140000",
-};
-console.log(worker);
 
-console.log("JOB:", worker.job);
+// const worker = {
+//   name: "Can",
+//   surname: "Canan",
+//   age: 33,
+//   job: "developer",
+//   languages: ["C++", "Java", "Javacript", "Pyhton", "Go"],
+//   salary: "140000",
+// };
+// console.log(worker);
 
-console.log("Lang:", worker["languages"]);
+// console.log("JOB:", worker.job);
 
-console.log(worker.languages[2]);
+// console.log("Lang:", worker["languages"]);
 
-worker["languages"].forEach((l) => console.log(l));
+// console.log(worker.languages[2]);
 
-worker.dob = "1990";
-worker.email = "can@gmail.com";
-console.log(worker);
+// worker["languages"].forEach((l) => console.log(l));
 
-// worker["salary"] = worker["salary"] * 1.1;
-worker["salary"] *= 1.1;
-console.log(worker);
+// worker.dob = "1990";
+// worker.email = "can@gmail.com";
+// console.log(worker);
 
-//* Object Copy
-const person = worker; //! Shallow (Sig - Sığ) Copying
-console.log("PERSON:", person);
+// // worker["salary"] = worker["salary"] * 1.1;
+// worker["salary"] *= 1.1;
+// console.log(worker);
+
+// //* Object Copy
+// const person = worker; //! Shallow (Sig - Sığ) Copying
+// console.log("PERSON:", person);
