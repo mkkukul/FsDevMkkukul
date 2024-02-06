2ad55cee58a2d926aa9a6af0d2d98424f476d82d
mkkukul <mkkukul@gmail.com>
2024-02-06 16:51:19 +0300

    15

--- JavaScript Basics/Series Js 11/objects.js
+++ JavaScript Basics/Series Js 11/objects.js
@@ -41,16 +41,16 @@ console.log(worker);
 // Nesne kopyalama
 const person = worker; // Sığ kopyalama
 console.log("PERSON:", person);
-
-console.log(worker): Çalışan nesnesi tüm detaylarıyla ekrana yazdırılır.
-console.log("JOB:", worker.job): Çalışanın iş bilgisi ekrana yazdırılır.
-console.log("Lang:", worker["languages"]): Çalışanın bildiği diller dizi olarak ekrana yazdırılır.
-console.log(worker.languages[2]): Çalışanın üçüncü dil bilgisi ekrana yazdırılır.
-worker["languages"].forEach((l) => console.log(l)): Çalışanın bildiği her dil ayrı ayrı ekrana yazdırılır.
-worker.dob = "1990"; ve worker.email = "can@gmail.com";: Çalışana doğum tarihi ve e-posta eklenir.
-Maaş artırma işlemi iki farklı yöntemle yapılır.
-const person = worker; ile sığ kopyalama yapılır ve bu kopya person değişkenine atanır.
-
+// 
+//  console.log(worker): Çalışan nesnesi tüm detaylarıyla ekrana yazdırılır.
+//  console.log("JOB:", worker.job): Çalışanın iş bilgisi ekrana yazdırılır.
+//  console.log("Lang:", worker["languages"]): Çalışanın bildiği diller dizi olarak ekrana yazdırılır.
+//  console.log(worker.languages[2]): Çalışanın üçüncü dil bilgisi ekrana yazdırılır.
+//  worker["languages"].forEach((l) => console.log(l)): Çalışanın bildiği her dil ayrı ayrı ekrana yazdırılır.
+//  worker.dob = "1990"; ve worker.email = "can@gmail.com";: Çalışana doğum tarihi ve e-posta eklenir.
+//  Maaş artırma işlemi iki farklı yöntemle yapılır.
+//  const person = worker; ile sığ kopyalama yapılır ve bu kopya person değişkenine atanır.
+// 
 // const worker = {
 //   name: "Can",
 //   surname: "Canan",
