daba000c752026f1c4461529378824c099775c65
mkkukul <mkkukul@gmail.com>
2024-02-08 19:36:19 +0300

    2

--- JavaScript Basics/Series Js 12/newOps.js
+++ JavaScript Basics/Series Js 12/newOps.js
@@ -266,6 +266,7 @@ console.log(p1, p2, p4); //* İlk, ikinci ve dördüncü öğeler ekrana yazdır
 //* REST: (Arrays)
 
 
+
 // const autos = ["anadol", "reno", "bmw", "mercedes", "ferrari"];
 // 
 // const [anadol, reno, ...restAutos] = autos;
