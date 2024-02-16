97c9f5348c6998e7e8d8a1f2d1d2b9ee70ec3408
mkkukul <mkkukul@gmail.com>
2024-02-16 21:11:09 +0300

    10

--- JavaScript Basics/Series Js 13/1-s2.js
+++ JavaScript Basics/Series Js 13/1-s2.js
@@ -8,5 +8,6 @@ console.log("***** SELECTORS *****");
 //*-------------------------------------------
 const h1 = document.getElementById("header")
 console.log(h1)
-h1.style.color = "red"
 h1.style.background = "white"
+h1.style.color = "red"
+
