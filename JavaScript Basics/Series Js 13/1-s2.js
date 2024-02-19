2d43791ad60ccc6b3dc8f505388568d8d8001974
mkkukul <mkkukul@gmail.com>
2024-02-19 20:56:15 +0300

    18

--- JavaScript Basics/Series Js 13/1-s2.js
+++ JavaScript Basics/Series Js 13/1-s2.js
@@ -132,6 +132,8 @@ itemList.style.background = "grey";
 // 
 //? 4- queryselector() ile CSS deki gibi secim yapilabilir.
 
+
+
 // const itemH2 = document.querySelector(".item-list h2");
 // itemH2.style.color = "purple";
 // 
