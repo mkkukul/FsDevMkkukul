58eaca9da419eca2497141eed7f07810b1d81c2b
mkkukul <mkkukul@gmail.com>
2024-02-20 22:13:55 +0300

    18

--- JavaScript Basics/Series Js 13/3-createNode.js
+++ JavaScript Basics/Series Js 13/3-createNode.js
@@ -2,6 +2,29 @@ console.log("**** CREATE NODE ****");
 
 //?Yeni bir P elementi olusturalim
 
+// Yeni bir <p> öğesi oluşturma
+const newP = document.createElement("p");
+
+// Yeni <p> öğesine id özelliği eklenmesi
+newP.id = "new-par";
+
+// Yeni <p> öğesine class özelliği eklenmesi
+newP.setAttribute("class", "new-class");
+
+// Yeni bir metin düğümü (text node) oluşturma ve içeriğini belirleme
+const text = document.createTextNode("This is a new p element");
+
+// Metin düğümünü (<p> öğesinin içeriği) yeni <p> öğesine eklenmesi
+newP.appendChild(text);
+
+// Oluşturulan yeni <p> öğesinin konsola yazdırılması
+console.log(newP);
+
+// HTML içerisindeki ".item-list h2" seçicisi ile hedeflenen h2 öğesine erişim sağlanması
+const h2 = document.querySelector(".item-list h2");
+
+// h2 öğesinin sonrasına (hemen sonra) yeni oluşturulan <p> öğesinin eklenmesi
+h2.after(newP);
 
 
 // const newP = document.createElement("p");
