431cddd014ea8461153aed10c3a8b046cfca23b4
mkkukul <mkkukul@gmail.com>
2024-02-20 22:13:44 +0300

    17

--- JavaScript Basics/Series Js 13/3-createNode.js
+++ JavaScript Basics/Series Js 13/3-createNode.js
@@ -4,15 +4,15 @@ console.log("**** CREATE NODE ****");
 
 
 
-const newP = document.createElement("p");
-newP.id = "new-par";
-newP.setAttribute("class", "new-class");
-const text = document.createTextNode("This is a new p element");
-newP.appendChild(text);
-console.log(newP);
-
-const h2 = document.querySelector(".item-list h2");
-h2.after(newP);
+// const newP = document.createElement("p");
+// newP.id = "new-par";
+// newP.setAttribute("class", "new-class");
+// const text = document.createTextNode("This is a new p element");
+// newP.appendChild(text);
+// console.log(newP);
+// 
+// const h2 = document.querySelector(".item-list h2");
+// h2.after(newP);
 
 //? getAttribute()
 console.log(newP.getAttribute("class"));
