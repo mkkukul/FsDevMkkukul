bcb05d2cbc69e4a4642805cf122aa289d0d9a947
mkkukul <mkkukul@gmail.com>
2024-03-01 19:07:55 +0300

    4

--- JavaScript Basics/Series Js 13/2-traversingDOM.js
+++ JavaScript Basics/Series Js 13/2-traversingDOM.js
@@ -84,7 +84,7 @@ console.log(ul.lastElementChild.innerText);
 //! const addItem = document.querySelector(".add-item");
 //! console.log(addItem.children);
 //! const h2 = addItem.children[0];
-// h2.style.color = "blue";
+//! h2.style.color = "blue";
 // 
 // console.log(addItem.parentNode.parentNode.children[0]); //? Header
 // 
