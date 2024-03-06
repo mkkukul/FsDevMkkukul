64dd54131d06f0d93211808118d8a11a674edfc7
mkkukul <mkkukul@gmail.com>
2024-03-06 14:57:07 +0300

    13

--- JavaScript Basics/Series Js 13/1-selectors.js
+++ JavaScript Basics/Series Js 13/1-selectors.js
@@ -115,9 +115,9 @@ list[4].innerHTML = "<a href='https://www.clarusway.com'>Clarusway Web</a>";
 
 //* EXAMPLE-4
 //* ------------------------------------------
-const myList = document.getElementsByClassName("list");
-console.log(myList); //? HTML Collection
-console.log(myList[0].innerText);
+// const myList = document.getElementsByClassName("list");
+// console.log(myList); //? HTML Collection
+// console.log(myList[0].innerText);
 
 //? HRML Collection'larda Dizi metotlari dogrudan kullanilamaz.
 //? Spread ve Array.from() ile kullanilabilri hale gelir.
