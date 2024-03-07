63ffb0ac962da3c3cc0b165f29215baf276b59be
mkkukul <mkkukul@gmail.com>
2024-03-07 22:31:29 +0300

    7

--- JavaScript Basics/Series Js 13/1-selectors.js
+++ JavaScript Basics/Series Js 13/1-selectors.js
@@ -161,6 +161,14 @@ myListArray.forEach((item) => console.log(item.innerText));
 
 //? 1- queryselector() etiket secebilir.
 
+// İlk <li> öğesini seçer
+const myLi = document.querySelector("li");
+
+// Seçilen <li> öğesinin iç metnini konsola yazdırır
+console.log(myLi.innerText);
+
+// Seçilen <li> öğesinin metninin rengini sarı yeşil (yellowgreen) olarak ayarlar
+myLi.style.color = "yellowgreen";
 
 
 // const myLi = document.querySelector("li");
