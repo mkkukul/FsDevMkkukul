a856d5cdf574985143d19b44418ab92af8301adb
mkkukul <mkkukul@gmail.com>
2024-03-08 16:31:14 +0300

    9

--- JavaScript Basics/Series Js 13/1-selectors.js
+++ JavaScript Basics/Series Js 13/1-selectors.js
@@ -230,12 +230,13 @@ console.log(myBtn);
 //*              QUERYSELECTORALL()
 //* ========================================
 
-const lists = document.querySelectorAll("ul li");
-console.log(lists); //? nodelist
-
-lists.forEach((li) => console.log(li.innerText));
-
-console.log(
-  document.querySelectorAll("section ul li:nth-child(3)")[0].innerText
-);
-console.log(document.querySelector("section ul li:nth-child(3)").innerText);
+// const lists = document.querySelectorAll("ul li");
+// console.log(lists); //? nodelist
+// 
+// lists.forEach((li) => console.log(li.innerText));
+// 
+// console.log(
+  // document.querySelectorAll("section ul li:nth-child(3)")[0].innerText
+// );
+// console.log(document.querySelector("section ul li:nth-child(3)").innerText);
+// 
