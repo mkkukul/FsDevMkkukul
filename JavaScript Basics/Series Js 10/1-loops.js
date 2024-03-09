a6f6454319a50c786ad8b3b2ee8b9318912e913d
mkkukul <mkkukul@gmail.com>
2024-03-09 22:12:45 +0300

    16

--- JavaScript Basics/Series Js 10/1-loops.js
+++ JavaScript Basics/Series Js 10/1-loops.js
@@ -62,7 +62,7 @@ console.log(dizi); // Orijinal dizi
 //! Bir notlar dizisi tanımlama
 const notlar = [55, 77, 23, 89, 100];
 
-// Toplamı saklamak için bir değişken tanımlama
+//! Toplamı saklamak için bir değişken tanımlama
 let toplam = 0;
 
 // Döngü kullanarak notları topla
