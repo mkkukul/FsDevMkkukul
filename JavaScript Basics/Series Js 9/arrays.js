f03bfaf3de69df267da724e6bc30dc0419db6c45
mkkukul <mkkukul@gmail.com>
2024-03-11 20:09:40 +0300

    18

--- JavaScript Basics/Series Js 9/arrays.js
+++ JavaScript Basics/Series Js 9/arrays.js
@@ -117,7 +117,7 @@ console.log(isimler); //! Güncellenmiş `isimler` dizisini konsola yazdırır.
 
 console.log(numbers); //! Diziyi konsola yazdırır. Örneğin, [3, 2, 1]
 numbers[numbers.length - 1]++; //! Dizinin son elemanını bir artırır. Bu örnekte, [3, 2, 2] olacaktır.
-console.log(numbers); // Güncellenmiş diziyi konsola yazdırır.
+console.log(numbers); //! Güncellenmiş diziyi konsola yazdırır.
 
 
 // console.log(numbers);
