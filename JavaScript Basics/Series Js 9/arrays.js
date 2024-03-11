c9088c934216ad2cfe5b76809c8c58200c0730b1
mkkukul <mkkukul@gmail.com>
2024-03-11 20:08:36 +0300

    13

--- JavaScript Basics/Series Js 9/arrays.js
+++ JavaScript Basics/Series Js 9/arrays.js
@@ -92,7 +92,7 @@ console.log(diller[1]); //! Dizinin ikinci elemanını (indeks 1) konsola yazdı
 const go = diller[3]; //! Dizinin dördüncü elemanını `go` adlı bir değişkene atar.
 console.log(go); //! `go` değişkenini konsola yazdırır.
 console.log(isimler[isimler.length - 1]); //! `isimler` dizisinin son elemanını konsola yazdırır.
-console.log(isimler.at(-1)); // `at()` metodu ile dizinin son elemanını konsola yazdırır. `-1` kullanarak sondan başa doğru indeksleme yapabilirsiniz.
+console.log(isimler.at(-1)); //! `at()` metodu ile dizinin son elemanını konsola yazdırır. `-1` kullanarak sondan başa doğru indeksleme yapabilirsiniz.
 isimler[1] = "saffet"; // `isimler` dizisinin ikinci elemanını "saffet" ile değiştirir.
 console.log(isimler); // Güncellenmiş `isimler` dizisini konsola yazdırır.
 
