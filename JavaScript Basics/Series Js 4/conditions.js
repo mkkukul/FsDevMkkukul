536c8fa02c2f1206f2cf55e7d682c8289e6d97c2
mkkukul <mkkukul@gmail.com>
2024-04-04 14:09:59 +0300

    17

--- JavaScript Basics/Series Js 4/conditions.js
+++ JavaScript Basics/Series Js 4/conditions.js
@@ -8,7 +8,7 @@ console.log("****** CONDITIONS *******");
 //? ORNEK1: Console'dan girilen bir sayisinin pozitif,negatif
 //! yada 0 oldugunu tespit ederek yazdiriniz.
 
-// Kullanıcıdan bir sayı al
+//! Kullanıcıdan bir sayı al
 const sayi = prompt("Bir sayı giriniz:");
 
 // Girilen sayının pozitif, negatif veya sıfır olduğunu kontrol et
